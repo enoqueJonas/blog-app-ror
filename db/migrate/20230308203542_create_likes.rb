41b67174acde88107a690f04a7583528616e169d
enoqueJonas <enoque.macanda@gmail.com>
2023-03-08 23:05:54 +0200

    UPDATE .rubocop.yml

--- db/migrate/20230308203542_create_likes.rb
+++ db/migrate/20230308203542_create_likes.rb
@@ -4,7 +4,7 @@ class CreateLikes < ActiveRecord::Migration[7.0]
       t.bigint :author_id
       t.bigint :post_id
       t.references :author_id, null: false, foreign_key: true
-      t.references :post-id, null: false, foreign_key: true
+      t.references :post - id, null: false, foreign_key: true
 
       t.timestamps
     end
