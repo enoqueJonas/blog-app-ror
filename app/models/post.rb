4aeb32b0f909e119dd9902abedb925a0d569450f
enoqueJonas <enoque.macanda@gmail.com>
2023-03-10 23:44:36 +0200

    UPDATE post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,15 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
 
-  def post_counter
-    user.update(post_counter: user.posts.count)
-  end
-
   def recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  after_save :post_counter
+
+  private
+
+  def post_counter
+    user.update(post_counter: user.posts.count)
   end
 end
