57193a8680e45ba0e85a9197adee4862afd955ca
enoqueJonas <enoque.macanda@gmail.com>
2023-03-10 20:14:37 +0200

    UPDATE tables foreign keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-    has_many :post 
-    has_many :like 
+    has_many :posts, foreign_key: "posts_id"
+    has_many :likes
     has_many :comment
+
+    def recent_posts
+        post.order(created_at: :desc).limit(3)
+    end
 end
