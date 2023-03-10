6114bd2e22cbc764ed3dc6322e22da73214a69dd
enoqueJonas <enoque.macanda@gmail.com>
2023-03-10 20:20:38 +0200

    ADD User#recent_posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    has_many :like 
-    has_many :comment 
-    belongs_to :user, foreign_key: "users_id" 
+    has_many :likes
+    has_many :comments
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-    has_many :posts, foreign_key: "posts_id"
+    has_many :posts
     has_many :likes
-    has_many :comment
+    has_many :comments
 
     def recent_posts
-        post.order(created_at: :desc).limit(3)
+        posts.order(created_at: :desc).limit(3)
     end
 end
