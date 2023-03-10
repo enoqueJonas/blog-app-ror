44cfa29cfb15e3ab455b4a6bff75c05526f9d066
enoqueJonas <enoque.macanda@gmail.com>
2023-03-11 00:23:29 +0200

    UPDATE User model adding some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :name, presence: true
+  validates :post_counter, numericality: {greater_than: 0, only_integer: true} 
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
