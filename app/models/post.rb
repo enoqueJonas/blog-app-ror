e1e0ad2392afe39b27898dbac25768d90f17f477
enoqueJonas <enoque.macanda@gmail.com>
2023-03-11 00:27:24 +0200

    UPDATE Post model adding some validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
 
+  validates :title, presence: true, length: {in: 1...250}
+  validates :comments_counter, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+  validates :likes_counter, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
