b3dd695fa3823074b6d6f46dfa07b49b4b2dd442
enoqueJonas <enoque.macanda@gmail.com>
2023-03-11 01:15:55 +0200

    ADD unit tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :comments
 
   validates :name, presence: true
-  validates :post_counter, numericality: {greater_than_or_equal_to: 0, only_integer: true} 
+  validates :post_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
