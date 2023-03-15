5170b9de06623a3244cf4501437e8305b62f5646
enoqueJonas <enoque.macanda@gmail.com>
2023-03-15 18:06:31 +0200

    ADD request test for Posts

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper.rb'
 
 RSpec.describe "Users", type: :request do
-    before (:example) { get users_path}
+    before (:example) { get users_path }
     
     it "is a success" do
         expect(response).to have_http_status(:ok)
@@ -13,5 +13,22 @@ RSpec.describe "Users", type: :request do
 
     it "shows correct boby placeholder text" do
         expect(response.body).to include "This Will display all users"
+    end
+end
+
+RSpec.describe "Posts", type: :request do
+
+    before (:example) { get user_posts_url(user_id: 1) }
+
+    it "is a success" do
+        expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+        expect(response).to render_template('index')
+    end 
+
+    it "shows correct body placeholder text" do
+        expect(response.body).to include "This Will show all posts"
     end
 end
