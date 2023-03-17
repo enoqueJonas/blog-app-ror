21a9bc2828db0074c978abc6a993de8853fd0bb9
enoqueJonas <enoque.macanda@gmail.com>
2023-03-17 20:11:21 +0200

    ADD functionality to add a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,22 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(params_provider)
+    @post.user = current_user
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    if @post.save
+      flash[:notice] = 'Post created successfully'
+      redirect_to user_posts_url(current_user)
+    else
+      flash[:alert] = 'Could not create post!'
+      render :new
+    end
+  end
+
+  def params_provider
+    params.require(:post).permit(:title, :text)
+  end
 end
