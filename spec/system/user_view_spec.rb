8f7c83bac05673f9db541226408d4abf4ffd884b
enoqueJonas <enoque.macanda@gmail.com>
2023-03-20 23:32:28 +0200

    ADD integration test to check for users photos in the index page

--- spec/system/user_view_spec.rb
+++ spec/system/user_view_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'User view', type:feature do
     before do
-        @user1 = User.create(name: 'Enoque')
+        @user1 = User.create(name: 'Enoque', photo:'example.jpg' )
         @user2 = User.create(name: 'Ana')
         @user3 = User.create(name: 'Emidio')
     end
@@ -13,6 +13,12 @@ RSpec.describe 'User view', type:feature do
             expect(page).to have_content(@user1.name)
             expect(page).to have_content(@user2.name)
             expect(page).to have_content(@user3.name)
+        end
+
+        it 'should display image' do
+            visit '/users'
+
+            expect(page).to have_selector("img[src='#{@user1.photo}']")
         end
     end
 end
