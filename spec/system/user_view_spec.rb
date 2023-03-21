486ff226b531e4e805f9a406414053f344ee0e2d
enoqueJonas <enoque.macanda@gmail.com>
2023-03-21 13:08:43 +0200

    ADD integration test to test prof pic display in user#show

--- spec/system/user_view_spec.rb
+++ spec/system/user_view_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'User view', type:feature do
+
     before(:each) do
         @user = User.create(name: 'Enoque', photo:'example.jpg',  bio: 'my bio', post_counter: 1)
         @post = Post.create(title: 'Post title', text: 'Post text', comments_counter: 1, likes_counter: 1, user_id: @user.id)
         @comment = Comment.create(text: 'Comment text', user_id: @user.id, post_id: @post.id)
         Like.create(user_id: @user.id, post_id: @post.id)
-        visit '/users'
     end
+
     describe 'Index page' do
+
+        before(:each) do
+            visit '/users'
+        end
+
         it 'should show all users name' do
             expect(page).to have_content(@user.name)
         end
@@ -25,6 +31,16 @@ RSpec.describe 'User view', type:feature do
             click_link 'Enoque'
 
             expect(page).to have_current_path "/users/#{@user.id}"
+        end
+    end
+
+    describe 'Show page' do
+        before(:each) do
+            visit "/users/#{@user.id}"
+        end
+
+        it 'Should show users profile pic' do
+            expect(page).to have_selector("img[src='#{@user.photo}']")
         end
     end
 end
